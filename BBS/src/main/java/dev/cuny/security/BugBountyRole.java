977da0839830c40ddcf48bf5d65a8ff165d06ed7
RevatureGentry <william.gentry@revature.com>
2020-10-05 15:47:30 -0400

    Deployment readiness
    - Align `BugBountyRole` with roles found in the frontend
    - Alter `JwtPresentFilter` to not check if token is present on login request
    - Add `demo` user with Admin Privileges

--- BBS/src/main/java/dev/cuny/security/BugBountyRole.java
+++ BBS/src/main/java/dev/cuny/security/BugBountyRole.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.GrantedAuthority;
  */
 public enum BugBountyRole implements GrantedAuthority {
 
+  ROLE_UNREGISTERED("ROLE_UNREGISTERED"),
+
   ROLE_USER("ROLE_USER"),
 
   ROLE_ADMIN("ROLE_ADMIN");
--- BBS/src/main/java/dev/cuny/security/JwtPresentFilter.java
+++ BBS/src/main/java/dev/cuny/security/JwtPresentFilter.java
@@ -6,6 +6,7 @@ import org.apache.maven.shared.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -36,6 +37,11 @@ public class JwtPresentFilter extends OncePerRequestFilter {
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    // Don't check for token when authenticating
+    if (HttpMethod.POST.name().equals(request.getMethod()) && "/login".equals(request.getRequestURI())) {
+      filterChain.doFilter(request, response);
+      return;
+    }
     logger.info("Incoming {} request to {}", request.getMethod(), request.getRequestURI());
     String header = request.getHeader(HttpHeaders.AUTHORIZATION);
     Authentication authentication = null;
